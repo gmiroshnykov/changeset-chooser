17ad404c7b7d909c94c1a806e115704c02a3767f
George Miroshnykov <george.miroshnykov@gmail.com>
2014-03-03 18:06:43 +0200

    Initial commit

---
+++ config.js
@@ -0,0 +1,10 @@
+var config = exports;
+config.HOST = process.env.HOST || '0.0.0.0';
+config.PORT = process.env.PORT || 5000;
+
+config.REPOSITORY = '/Users/laggyluke/Projects/playground-central';
+
+config.REVIEWBOARD_URL = 'http://rb.dev';
+config.REVIEWBOARD_USERNAME = 'admin';
+config.REVIEWBOARD_PASSWORD = 'admin';
+config.REVIEWBOARD_REPOSITORY = 1;
